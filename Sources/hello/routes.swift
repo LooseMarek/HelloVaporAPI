07a41f620a94a7895d1223e211794009bc73f447
Marek Loose <loose.marek@gmail.com>
2025-09-05 09:52:45 +0200

    Exploring Routes

--- Sources/hello/routes.swift
+++ Sources/hello/routes.swift
@@ -4,8 +4,38 @@ func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
     }
-
+    
     app.get("hello") { req async -> String in
-        "Hello, world!"
+        "Hello, World!"
+    }
+
+    app.get("hello", ":name") { req async -> String in
+        let name = req.parameters.get("name") ?? "World"
+        return "Hello, \(name)!"
+    }
+    
+    app.get("number", ":x") { req -> String in
+        guard let int = req.parameters.get("x", as: Int.self) else {
+            throw Abort(.badRequest)
+        }
+        return "\(int) is a great number"
+    }
+    
+    let users = app.grouped("users")
+    
+    // GET /users
+    users.get { req in
+        ""
+    }
+    
+    // POST /users
+    users.post { req in
+        ""
+    }
+    
+    // GET /users/:id
+    users.get(":id") { req in
+        let id = req.parameters.get("id")!
+        return ""
     }
 }
